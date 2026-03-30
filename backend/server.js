71fd8a6772f4dfa3a29fd101e4c9bae9e751fb0c
HarshaJayaweera21 <harshajayaweera1965@gmail.com>
2026-03-30 16:03:48 +0530

    initialized backend structure with Express, MongoDB connection, and user/role models

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB connection
+connectDB();
+
+
+app.get('/', (req, res) => {
+    // res.send("<h1>Hello World</h1>");
+    res.json({message: "UniStay API is running"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`)
+})
+
+
+
+
---
+++ backend/src/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum:["male", "female", "other"],
+        lowercase: true,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique:true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique:true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        select: false
+    },
+    role: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role",
+        required: true
+    }
+
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
