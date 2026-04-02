d9fc72c51f056bd25cffc9f5d750f1cb0ea07f46
HarshaJayaweera21 <harshajayaweera1965@gmail.com>
2026-04-02 11:44:51 +0530

    add authentication middleware for JWT verification and role authorization

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,51 @@
+const jwt = require("jsonwebtoken");
+
+// Check if user is logged in
+const protect = (req, res, next) => {
+    let token;
+
+    // Check Authorization header
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Get token
+            token = req.headers.authorization.split(" ")[1];
+
+            // verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Attach user info to request
+            req.user = decoded;
+
+            next();
+        } catch(error) {
+            return res.status(401).json({
+                success: false,
+                message: "Not authorized, token failed"
+            });
+        }
+    }
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Not authorized, no token"
+        });
+    }
+}
+
+// check if user has the right role
+const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: "Access denied"
+            })
+        }
+    }
+}
+
+module.exports = {protect, authorizeRoles};
